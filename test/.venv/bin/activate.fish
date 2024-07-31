85806cedc8b5148ccbd355ddf25c44629e6060f3
Josef Emanuele Zerpa Ruiz <zerparuiz.1837394@studenti.uniroma1.it>
2024-07-31 10:55:27 +0200

    Update ignore

--- test/.venv/bin/activate.fish
+++ test/.venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/macbookpro/josef/softenglab/local/.venv"
+set -gx VIRTUAL_ENV "/Users/macbookpro/josef/softenglab/test/.venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
