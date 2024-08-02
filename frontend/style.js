f6fcdaa4acd0249c39be343c78be005f21d6ca47
Ale110901 <alececchetto2001@gmail.com>
2024-08-02 12:16:22 +0200

    added style properties

--- frontend/style.js
+++ frontend/style.js
@@ -4,10 +4,27 @@ export default StyleSheet.create({
 	mb20: {
 		marginBottom: 20
 	},
+	mb10: {
+		marginBottom: 10
+	},
 	mt20: {
 		marginTop: 20
 	},
 	spaced: {
 		margin: 50
+	},
+	ml10: {
+		marginLeft: 10
+	},
+	card: {
+		margin: 15,
+		shadowColor: "#000000",
+		shadowOffset: {
+			width: 0,
+			height: 5,
+		},
+		shadowOpacity:  0.20,
+		shadowRadius: 5.62,
+		elevation: 7
 	}
 });
