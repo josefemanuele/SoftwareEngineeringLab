db2727d6362d9a2e4d913bedc0493e236926fab2
Josef Emanuele Zerpa Ruiz <zerparuiz.1837394@studenti.uniroma1.it>
2024-08-05 18:22:50 +0200

    Move flask_app under test folder. Add organization service. Edit user service

---
+++ services/organization/src/main.py
@@ -0,0 +1,43 @@
+from flask import Flask, jsonify, request
+from flask_restful import Api, Resource
+
+app = Flask(__name__)
+id_counter = 2
+
+organizations = {
+  "barber_shop" : {'organization_id' : 1, 'owner_id' : '2'},
+  "sapienza": {'organization_id' : 2, 'owner_id' : '1'}
+}
+
+@app.route("/")
+def hello_world():
+    return "Prenotalo!"
+
+@app.route('/getbyuser/<int:user_id>', methods=['GET'])
+def getbyuser(user_id):
+    global organizations
+    match = {k : v for k, v in organizations.items() if int(v['owner_id']) == user_id }
+    return jsonify(match)
+
+@app.route('/create', methods=['POST'])
+def create():
+    data = request.json
+    organization = data['organization']
+    owner_id = data['owner_id']
+    global organizations
+    match = organizations.get(organization)
+    if (match is None):
+        global id_counter
+        id_counter += 1
+        '''
+        new_user = User(id = id_counter, username = user, password = pwd)
+        db.create_all()
+        db.session.add(new_user)
+        db.session.commit()
+        print(UserSchema(many=True).dump(User.query.all()))
+        '''
+        organizations[organization] = {"id" : id_counter, "owner_id" : owner_id}
+        print(organizations)
+        return "Organization created."
+    
+    return "Error"
