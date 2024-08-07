ece1c63f72e4cf29f94df425a0fab75b7a85b608
Hackfront <hackfront@protonmail.ch>
2024-08-07 13:26:42 +0200

    Change app header icons

--- frontend/components/AppHeader.js
+++ frontend/components/AppHeader.js
@@ -5,12 +5,17 @@ import { Appbar } from 'react-native-paper';
 import { doLogout } from '../lib/user.js';
 
 export default function AppHeader({ navigation, route, options, back }) {
+	let notifNum = 1;
+
+	let notifIcon = notifNum > 0 ? 'message-badge-outline' : 'message-outline';
+	let backButton = back ? <Appbar.BackAction onPress={navigation.goBack} /> : null;
+
 	return (
 		<Appbar.Header>
-			{ back ? <Appbar.BackAction onPress={navigation.goBack} /> : null}
-			<Appbar.Content title="Prenotalo" />
-			<Appbar.Action icon="abacus" />
-			<Appbar.Action icon="account-box" onPress={doLogout} />
+			{backButton}
+			<Appbar.Content title={route.name} />
+			<Appbar.Action icon={notifIcon} />
+			<Appbar.Action icon="account-circle" onPress={doLogout} />
 		</Appbar.Header>
 	)
 }
