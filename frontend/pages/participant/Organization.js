107a19786e237b6f6627a023cb89a61c56c7a899
Hackfront <hackfront@protonmail.ch>
2024-08-12 15:14:36 +0200

    Add event fetch in participant Organization page

--- frontend/pages/participant/Organization.js
+++ frontend/pages/participant/Organization.js
@@ -1,33 +1,78 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { RefreshControl, ScrollView, View } from 'react-native';
+import { Card, Divider, Text } from 'react-native-paper';
 
-import { Card, Text } from 'react-native-paper';
+import { doRequest } from '../../lib/rest.js';
 
-import style from '../../style.js';
+import { ids as bsIds, styles as bsStyles } from '../../style/bootstrap.js';
+import style from '../../style/custom.js';
 
 export default function Organization({ navigation, route }) {
-	let params = route.params;
+	let { params } = route;
 
-	let event = {
-		id: 432,
-		title: 'Evento a caso',
-		subtitle: 'Sottotitolo',
-		content: 'Descrizione',
-	};
+  let [ events, setEvents ] = useState([]);
 
-	return (
-		<>
-			<Text>Organization {params.id} page!</Text>
+  let [ refreshing, setRefreshing ] = useState(false);
 
-			<Card key={event.id} style={style.card} onPress={() => navigation.push('participant/Event', {
-				id: event.id
-			})}>
-				<Card.Title title={event.title} subtitle={event.subtitle}
-					titleStyle={{ fontWeight: 'bold' }}
-				/>
-				<Card.Content>
-					<Text>{event.content}</Text>
-				</Card.Content>
-			</Card>
-		</>
-	);
+  async function doRefresh() {
+    setRefreshing(true);
+
+    let response;
+    try {
+      response = await doRequest('event', 'GET', `/organization/${params.id}/event`, null);
+    } catch (e) {
+      // nothing
+    }
+
+    if (response != null) {
+      setEvents(response);
+    } else {
+      console.log('Error fetching event list')
+    }
+
+    setRefreshing(false);
+  }
+
+  useEffect(() => {
+    doRefresh();
+  }, []);
+
+  return (
+    <>
+      <ScrollView refreshControl={
+        <RefreshControl refreshing={refreshing} onRefresh={doRefresh} />
+      }>
+				<Card key="details" style={style.card}>
+					<Card.Title title='Details' titleStyle={{ fontWeight: 'bold' }} />
+					<Card.Content>
+						<Text>
+							Description: prova
+						</Text>
+					</Card.Content>
+					{/* <Card.Actions>
+						<Button>Prenota</Button>
+					</Card.Actions> */}
+				</Card>
+
+				<Divider style={[ style.mt20, style.mb20 ]} />
+
+        {events.map((event) => (
+          <Card key={event.id} style={style.card} onPress={() => navigation.push('participant/Event', {
+						organization_id: params.id,
+            event_id: event.id,
+          })}>
+            <Card.Title title={event.name} subtitle={event.category}
+              titleStyle={{ fontWeight: 'bold' }}
+            />
+            <Card.Content>
+              <Text>{event.description}</Text>
+            </Card.Content>
+						{/* <Card.Actions>
+							<Button>Prenota</Button>
+						</Card.Actions> */}
+          </Card>
+        ))}
+      </ScrollView>
+    </>
+  );
 }
