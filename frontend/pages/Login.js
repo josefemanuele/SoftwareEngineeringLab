a41475764d90a776f34e6481e527179ea296eaa3
Hackfront <hackfront@protonmail.ch>
2024-08-12 11:49:37 +0200

    Add loading animation and network error to Login screen

--- frontend/pages/Login.js
+++ frontend/pages/Login.js
@@ -5,33 +5,50 @@ import { Appbar, Button, Divider, HelperText, Switch, Text, TextInput } from 're
 import { doLogin } from '../lib/user.js';
 import style from '../style.js';
 
+let loginErrors = {
+  'network': 'Network error',
+  'invalid': 'Wrong email or password',
+}
+
 export default function Login({ navigation }) {
   let [ email, setEmail ] = useState('');
   let [ password, setPassword ] = useState('');
   let [ staySignedIn, setStaySignedIn ] = useState(false);
 
   let [ emailError, setEmailError ] = useState(false);
-  let [ loginError, setLoginError ] = useState(false);
+
+  let [ loginError, setLoginError ] = useState('');
+  let [ loading, setLoading ] = useState(false);
 
   async function onLogin() {
     if (emailError) {
       return;
     }
 
-    let loginOk = await doLogin(email, password);
+    setLoading(true);
 
-    if (!loginOk) {
-      setLoginError(true);
+    let loginOk;
+    try {
+      loginOk = await doLogin(email, password);
+
+      if (!loginOk) {
+        setLoginError('invalid');
+      }
+    } catch (err) {
+      setLoginError('network');
     }
+
+    setLoading(false);
   };
 
   return (
-    <View style={style.spaced}>
+    <View style={[ style.spaced, style.container ]}>
       <TextInput label="E-mail" value={email} error={emailError}
         onChangeText={text => {
           let validEmail = text.length > 0 &&
             !!(/^[a-zA-Z0-9._]+\@[a-zA-Z0-9.]+$/.test(text));
-          
+
+          setLoginError('');
           setEmailError(!validEmail);
           setEmail(text);
         }}
@@ -43,7 +60,7 @@ export default function Login({ navigation }) {
 
       <TextInput label="Password" value={password} secureTextEntry={true}
         onChangeText={text => {
-          setLoginError(false);
+          setLoginError('');
           setPassword(text);
         }}
         onKeyPress={event => {
@@ -53,8 +70,8 @@ export default function Login({ navigation }) {
         }}
       />
 
-      <HelperText type="error" visible={loginError}>
-        Wrong email or password
+      <HelperText type="error" visible={loginError !== ''}>
+        {loginErrors[loginError]}
       </HelperText>
 
       <View style={{ flexDirection: 'row', alignItems: 'center' }}>
@@ -66,7 +83,7 @@ export default function Login({ navigation }) {
         Forgot password?
       </Button>
 
-      <Button title="Login" mode="contained" onPress={onLogin}>
+      <Button title="Login" mode="contained" loading={loading} onPress={onLogin}>
         Login
       </Button>
 
