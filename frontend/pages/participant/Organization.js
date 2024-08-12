ae64154418b5a9840be4d907097bb23ad38505d4
Hackfront <hackfront@protonmail.ch>
2024-08-12 11:51:22 +0200

    Add event card to participant Organization screen

--- frontend/pages/participant/Organization.js
+++ frontend/pages/participant/Organization.js
@@ -1,13 +1,33 @@
 import React from 'react';
 
-import { Text } from 'react-native-paper';
+import { Card, Text } from 'react-native-paper';
 
-export default function Organization({ route }) {
+import style from '../../style.js';
+
+export default function Organization({ navigation, route }) {
 	let params = route.params;
+
+	let event = {
+		id: 432,
+		title: 'Evento a caso',
+		subtitle: 'Sottotitolo',
+		content: 'Descrizione',
+	};
 
 	return (
 		<>
 			<Text>Organization {params.id} page!</Text>
+
+			<Card key={event.id} style={style.card} onPress={() => navigation.push('participant/Event', {
+				id: event.id
+			})}>
+				<Card.Title title={event.title} subtitle={event.subtitle}
+					titleStyle={{ fontWeight: 'bold' }}
+				/>
+				<Card.Content>
+					<Text>{event.content}</Text>
+				</Card.Content>
+			</Card>
 		</>
 	);
 }
