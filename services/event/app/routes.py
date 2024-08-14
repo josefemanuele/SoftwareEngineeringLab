9e4ab5bb9e2a44054ce78a9d8a9bc3b5bf7abfef
Josef Emanuele Zerpa Ruiz <zerparuiz.1837394@studenti.uniroma1.it>
2024-08-14 07:12:01 +0200

    Add event tests. Fix routes logic and refactor

--- services/event/app/routes.py
+++ services/event/app/routes.py
@@ -1,26 +1,34 @@
+from app import app
 from flask import jsonify, request
 
 events = {
-  1 : {'id' : 1, 'time' : 10, 'description' : 'Something fun'},
-  2 : {'id' : 2, 'time' : 12, 'description' : 'Something funnier'}
+  1 : {'id' : 1, 'organization_id' : 2, 'time' : 10, 'description' : 'Something fun'},
+  2 : {'id' : 2, 'organization_id': 2, 'time' : 12, 'description' : 'Something funnier'}
 }
 id_counter = 2
 
 @app.route('/getbyorganization/<int:organization_id>', methods=['GET'])
-def get_by_user(organization_id):
+def getByOrganization(organization_id):
     global events
     match = {k : v for k, v in events.items() if int(v['organization_id']) == organization_id }
     return jsonify(match)
 
+@app.route('/getupcoming/<int:time>', methods=['GET'])
+def getUpcoming(time):
+    global events
+    match = {k : v for k, v in events.items() if int(v['time']) >= time }
+    return jsonify(match)
+
 @app.route('/create', methods=['POST'])
 def create():
+    global events
+    global id_counter
     data = request.json
     organization_id = data['organization_id']
     time = data['time']
     description = data['description']
-    global id_counter
     id_counter += 1
-    global events
-    events[id_counter] = {'organization_id' : organization_id, 'time' : time, 'description' : description},
+    event = {'id' : id_counter, 'organization_id' : organization_id, 'time' : time, 'description' : description}
+    events[id_counter] = event
     print(events)
-    return "Event created."
+    return jsonify(event)
