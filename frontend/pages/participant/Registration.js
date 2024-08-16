afc4ab8b0b138090c2aeef117ac6e0339927ce2a
Hackfront <hackfront@protonmail.ch>
2024-08-16 14:36:53 +0200

    Redo participant/Registration with use-form

--- frontend/pages/participant/Registration.js
+++ frontend/pages/participant/Registration.js
@@ -1,68 +1,131 @@
-import React, { useState } from 'react';
-import { View } from 'react-native';
-import { Text, TextInput, Button, Divider, Modal, Portal } from 'react-native-paper';
-import { useNavigation } from '@react-navigation/native';
+import React, { useRef, useState } from 'react';
 
-import { doRegistration, doLogin } from '../../lib/user.js';
+import { ScrollView } from 'react-native';
+import { Button, Divider, Text, TextInput } from 'react-native-paper';
+import { en, registerTranslation, registerDefaultLocale, useFormState, Form } from 'react-native-use-form';
+
+import FullDialog from '../../components/FullDialog.js';
+import InputWithError from '../../components/InputWithError.js';
 
 import { ids as bsIds, styles as bsStyles } from '../../style/bootstrap.js';
 import style from '../../style/custom.js';
 
-export default function Registration({ navigation }) {
-  let [ email, setEmail ] = useState('');
-  let [ password, setPassword ] = useState('');
-  let [ name, setName ] = useState('');
-  let [ surname, setSurname ] = useState('');
+registerTranslation('en', en);
+registerDefaultLocale('en');
 
-  let [ modalVisible, setModalVisible ] = useState(false);
+const DIALOG_MESSAGES = {
+  null: {
+    title: '',
+    description: '',
+  },
+  'success': {
+    title: 'Account created',
+    description: 'Your user account has been registered. Login with your credentials and start using the app!'
+  },
+  'exists': {
+    title: 'Account exists',
+    description: 'An account with the same email address already exists. Use another email address or recover the password in the login page.'
+  },
+}
+
+export default function Registration({ navigation }) {
+  let [ loading, setLoading ] = useState(false);
+  let [ dialogMessage, setDialogMessage ] = useState(null);
+
+  let scrollViewRef = useRef(null);
+
+  let [{ errors, submit, formProps, hasError, values }, fh] = useFormState({
+    email: '',
+    passwordA: '',
+    passwordB: '',
+    name: '',
+    surname: '',
+  }, {
+    scrollViewRef: scrollViewRef,
+    onSubmit: values => {
+      setLoading(true);
+
+      // do registration
+
+      setTimeout(() => {
+        setLoading(false);
+        setDialogMessage('success');
+      }, 1000);
+    }
+  });
+
+  function onDialogDismiss() {
+    setDialogMessage(null);
+
+    if (dialogMessage === 'success') {
+      navigation.navigate('Login');
+    }
+  }
 
   return (
-    <View style={[ bsStyles.container ]} dataSet={{ media: bsIds.container }}>
-      <TextInput label="E-mail" value={email}
-        onChangeText={text => setEmail(text)}
-        style={{ marginBottom: 20 }}
-        keyboardType='email-address'
+    <>
+      <ScrollView ref={scrollViewRef} style={[ bsStyles.container ]} dataSet={{ media: bsIds.container }}>
+        <Form {...formProps}>
+          <InputWithError Component={TextInput} {...fh.email('email', {
+            required: true,
+            label: 'Email *',
+            shouldFollowRegexes: [
+              { regex: /^[a-zA-Z0-9._]+\@[a-zA-Z0-9.]+$/, errorMessage: 'Invalid email' },
+            ],
+          })} disabled={loading} />
+
+          <InputWithError Component={TextInput} {...fh.password('passwordA', {
+            required: true,
+            label: 'Password *',
+            minLength: 8,
+            maxLength: 128,
+            shouldFollowRegexes: [
+              { regex: /^[a-zA-Z0-9._]+$/, errorMessage: 'Password must contain only: a-z A-Z 0-9' },
+            ],
+          })} disabled={loading} />
+
+          <InputWithError Component={TextInput} {...fh.password('passwordB', {
+            required: true,
+            label: 'Confirm password *',
+            validate: value => {
+              if (value !== values.passwordA) {
+                return "Passwords don't match";
+              }
+
+              return true;
+            }
+          })} disabled={loading} />
+
+          <Divider style={[ style.mb20 ]} />
+
+          <InputWithError Component={TextInput} {...fh.text('name', {
+            required: true,
+            label: 'Name *',
+          })} disabled={loading} />
+
+          <InputWithError Component={TextInput} {...fh.text('surname', {
+            required: true,
+            label: 'Surname *',
+          })} disabled={loading} />
+
+          <Text variant="labelLarge" style={style.mt20}>All fields with an asterisk (*) are mandatory</Text>
+
+          <Button title="Register" mode="contained" style={style.mt20} loading={loading} onPress={submit} disabled={loading}>
+            Register
+          </Button>
+        </Form>
+      </ScrollView>
+
+      <FullDialog
+        title={DIALOG_MESSAGES[dialogMessage].title}
+        content={DIALOG_MESSAGES[dialogMessage].description}
+        actions={[{
+          name: 'OK',
+          callback: onDialogDismiss,
+        }]}
+        visible={dialogMessage != null}
+        onDismiss={onDialogDismiss}
       />
-
-      <TextInput label="Password" value={password}
-        onChangeText={text => setPassword(text)}
-        secureTextEntry={true}
-        style={{ marginBottom: 20 }}
-      />
-
-      <TextInput label="Name" value={name}
-        onChangeText={text => setName(text)}
-        style={{ marginBottom: 20 }}
-      />
-
-      <TextInput label="Surname" value={surname}
-        onChangeText={text => setSurname(text)}
-        style={{ marginBottom: 20 }}
-      />
-
-      <Button title="Register" mode="contained" onPress={async function () {
-        let userData = { name, surname, email, password };
-
-        let registrationOk = await doRegistration(userData);
-        if (!registrationOk) {
-          console.log('Registrazione fallita');
-          setModalVisible(true);
-
-          return;
-        }
-
-        console.log('Registrazione corretta');
-
-        let loginOk = await doLogin(email, password);
-        if (!loginOk) {
-          console.log('Login fallito');
-          setModalVisible(true);
-
-          return;
-        }
-      }}>
-        Register
-      </Button>
-    </View>
+    </>
   );
 }
