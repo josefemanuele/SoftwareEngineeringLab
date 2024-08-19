dc836bf2aae5bad5f3f76ed94eae734215d161b8
silviodantonio <64018191+silviodantonio@users.noreply.github.com>
2024-08-19 17:27:46 +0200

    Event service: refine json response and endpoints logic

--- services/event/src/db.py
+++ services/event/src/db.py
@@ -11,24 +11,42 @@ class Event(Base):
 
     id: Mapped[int] = mapped_column(primary_key=True)
     # For now these are all nullable
-    title: Mapped[str | None]
     organization_id: Mapped[int | None]
+    title: Mapped[str | None]
+    description: Mapped[str | None]
+    date: Mapped[str | None]
+    start_time: Mapped[str | None]
+    end_time: Mapped[str | None]
+    price: Mapped[float | None]
+    capacity: Mapped[int | None]
 
     def __repr__(self):
-        return f"Event(id={self.id}, title={self.title}, organization_id={self.organization_id})"
+        return f"{{ id: {self.id}, title: {self.title} }}"
 
 # Generating schemas in db
 Base.metadata.create_all(engine) 
 
 # DB functions for the whole collection
-def create_event(title: str) -> int:
+def create_event(**kwargs) -> int:
+
+    required_fields = ["title", "organization_id"]
+
+    for field in required_fields:
+        if field not in kwargs:
+            raise ValueError
+        
+    new_event = Event(title=kwargs["title"], 
+                      organization_id=kwargs["organization_id"])
+    
     with Session(engine) as session:
-        new_event = Event(title=title)
         session.add(new_event)
         session.commit()
         return new_event.id
 
 def get_all_events():
+    # This query should return a list of events.
+    # Events should be represented by a dictionary
+
     with Session(engine) as session:
         query = select(Event)
         return_dict = {}
--- services/event/src/main.py
+++ services/event/src/main.py
@@ -8,24 +8,38 @@ api = Api(app)
 parser = reqparse.RequestParser()
 parser.add_argument('event_desc', type=str)
 parser.add_argument('organization_id', type=int, location='args')
+parser.add_argument('event_data', type=dict, location='json')
 
 class EventsCollection(Resource):
 
     def get(self):
 
         args = parser.parse_args()
-        organization_id = args.organization_id['organization_id']
+        organization_id = args.organization_id
 
         if organization_id is not None:
-            return db.get_all_events_by_organization_id(organization_id)
+                return db.get_all_events_by_organization_id(organization_id)
         else:
             return db.get_all_events()
     
     # POST is for creating a new event
     def post(self):
+
         args = parser.parse_args()
-        event_desc = args['event_desc']
-        return db.create_event(event_desc)
+
+        print(args.event_data)
+
+        if args.event_data is None:
+            # 400: Bad request
+            return '', 400 
+        
+        event_data = args.event_data
+        # 201: Created
+        try:
+            new_event_id = db.create_event(**event_data)
+            return new_event_id, 201
+        except ValueError:
+            return '', 400
 
 class Event(Resource):
     def get(self, event_id):
@@ -42,8 +56,8 @@ class Event(Resource):
         db.delete_event(event_id)
         return '', 200
 
-api.add_resource(EventsCollection, "/")
-api.add_resource(Event, "/<string:event_id>")
+api.add_resource(EventsCollection, "/events")
+api.add_resource(Event, "/event/<string:event_id>")
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
