6cd5cb04d6d5a02d963aa6ac55fd269f5721fa7b
Ale110901 <alececchetto2001@gmail.com>
2024-08-24 18:57:42 +0200

    created payment page

---
+++ frontend/pages/participant/Payment.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Text } from 'react-native-paper'
+
+import { ids as bsIds, styles as bsStyles } from '../style/bootstrap.js';
+import style from '../style/custom.js';
+
+export default function Payment({ navigation, route }) {
+	return (
+		<>
+			<Text>Payment page!</Text>
+		</>
+	);
+}
