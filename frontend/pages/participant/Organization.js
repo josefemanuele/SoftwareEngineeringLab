9e8417bda568797428c5d946a37d5afbb4d2fb9d
Hackfront <hackfront@protonmail.ch>
2024-08-27 17:13:42 +0200

    Restyle participant/Organization page

--- frontend/pages/participant/Organization.js
+++ frontend/pages/participant/Organization.js
@@ -65,20 +65,22 @@ export default function Organization({ navigation, route }) {
         <RefreshControl refreshing={refreshing} onRefresh={doRefresh} />
       }>
 				<Card key="details">
-					<Card.Title  left={(props) => <Avatar.Icon {...props} icon="store" />} title={info.name} titleVariant="headlineSmall" titleStyle={{ fontWeight: 'bold' }} />
+					<Card.Title
+						title={info.name}
+						titleVariant="headlineSmall"
+						titleStyle={{ fontWeight: 'bold' }}
+						left={(props) => <Avatar.Icon {...props} icon="store" />}
+					/>
 					<Card.Content>
-						<Text style={{ marginBottom: 10 }}>
+						<View style={{ flexDirection: 'row', marginBottom: 10 }}>
               <Text style={{ fontWeight: 'bold' }}>Category: </Text>
-              {info.category}
-            </Text>
-						<Text>
+              <Text>{info.category}</Text>
+            </View>
+						<View style={{ flexDirection: 'row', marginBottom: 10 }}>
               <Text style={{ fontWeight: 'bold' }}>Info: </Text>
-              {info.description}
-            </Text>
+              <Text>{info.description}</Text>
+            </View>
 					</Card.Content>
-					{/* <Card.Actions>
-						<Button>Prenota</Button>
-					</Card.Actions> */}
 				</Card>
 
 				<Divider style={[ style.spaceTop, style.spaceBottom ]} />
@@ -87,27 +89,31 @@ export default function Organization({ navigation, route }) {
           <Card key={event.id} style={{
 						marginBottom: GLOBAL_SPACING,
 					}}>
-            <Card.Title title={event.name} titleStyle={{ fontWeight: 'bold', fontSize: 22 }}/>
+            <Card.Title title={event.name} titleStyle={{ fontWeight: 'bold', fontSize: 22 }} />
             <Card.Content>
-              <View style={{ flexDirection: 'row', marginBottom: 5}}>
-                  <Text style={{ marginRight: 4, fontWeight: 'bold' }}>Category: </Text>
-                  <Text>{event.category}</Text>
+							<View style={{ flexDirection: 'row', marginBottom: 5 }}>
+                <Text style={{ marginRight: 4, fontWeight: 'bold' }}>Category: </Text>
+                <Text>{event.category}</Text>
+              </View>
+							<View style={{ flexDirection: 'row', marginBottom: 5 }}>
+                <Text style={{ marginRight: 4, fontWeight: 'bold' }}>Date: </Text>
+                <Text>{event.date}</Text>
               </View>
               <View style={{ flexDirection: 'row', marginBottom: 5 }}>
-                  <Text style={{ marginRight: 4, fontWeight: 'bold' }}>Description: </Text>
-                  <Text>{event.description}</Text>
+                <Text style={{ marginRight: 4, fontWeight: 'bold' }}>Time: </Text>
+                <Text>{event.start_time} - {event.end_time}</Text>
+              </View>
+							<View style={{ flexDirection: 'row', marginBottom: 5 }}>
+                <Text style={{ marginRight: 4, fontWeight: 'bold' }}>Location: </Text>
+                <Text>{event.location}</Text>
+              </View>
+							<View style={{ flexDirection: 'row', marginBottom: 5 }}>
+                <Text style={{ marginRight: 4, fontWeight: 'bold' }}>Price: </Text>
+                <Text>{event.price}€</Text>
               </View>
               <View style={{ flexDirection: 'row', marginBottom: 5 }}>
-                  <Text style={{ marginRight: 4, fontWeight: 'bold' }}>Date: </Text>
-                  <Text>{event.date}</Text>
-              </View>
-              <View style={{ flexDirection: 'row', marginBottom: 5 }}>
-                  <Text style={{ marginRight: 4, fontWeight: 'bold' }}>Time: </Text>
-                  <Text>{event.start_time} - {event.end_time}</Text>
-              </View>
-              <View style={{ flexDirection: 'row'}}>
-                  <Text style={{ marginRight: 4, fontWeight: 'bold' }}>Price: </Text>
-                  <Text>{event.price}€</Text>
+                <Text style={{ marginRight: 4, fontWeight: 'bold' }}>Description: </Text>
+                <Text>{event.description}</Text>
               </View>
             </Card.Content>
 						<Card.Actions>
