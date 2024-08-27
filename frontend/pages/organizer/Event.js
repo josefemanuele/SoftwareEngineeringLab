a17d6a9f56c9940b2e905ae3ccaf87add475b723
Ale110901 <alececchetto2001@gmail.com>
2024-08-27 18:25:11 +0200

    added cancel button control

--- frontend/pages/organizer/Event.js
+++ frontend/pages/organizer/Event.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect }from 'react';
 
+import FullDialog from '../../components/FullDialog.js';
+
 import { View } from 'react-native';
 import { Button, Text, Icon } from 'react-native-paper';
 
@@ -7,7 +9,7 @@ import style from '../../style/custom.js';
 
 export default function EventPage({ navigation, route }) {
 	let params = route.params;
-    let [ dialogMessage, setDialogMessage ] = useState(null);
+    let [ dialogVisible, setDialogVisible ] = useState(false);
 
     let title = 'title'
     let description = 'description'
@@ -19,6 +21,8 @@ export default function EventPage({ navigation, route }) {
     let reservations = 20
 
     let [ booking, setBooking ] = useState(true);
+    let [ cancel, setCancel ] = useState(true);
+
 
 
     const checkAvailability = (value) => {
@@ -32,7 +36,18 @@ export default function EventPage({ navigation, route }) {
     useEffect(() => {
         checkAvailability(reservations);
     }, [reservations]);
+    
+    const checkCancel = (value) => {
+        if (value >= 1) { 
+          setCancel(false);
+        } else {
+          setCancel(true);
+        }
+    };
 
+    useEffect(() => {
+        checkCancel(reservations);
+    }, [reservations]);
 
 
 	return (
@@ -87,9 +102,20 @@ export default function EventPage({ navigation, route }) {
                         icon="close-circle-outline"
                         mode="elevated"
                         style={{ margin: 20 }}
-                        disabled={!booking}
+                        disabled={!cancel}
                         onPress={() => console.log('cancel event')}
                     >Cancel</Button>
+                    
+                    <FullDialog /* TODO */
+                        title="Confirmation message"
+                        content={`Do you want to cancel the event?`}
+                        actions={[{
+                            name: 'Yes',
+                            callback: () => setDialogVisible(false)
+                        }]}
+                        visible={dialogVisible}
+                        onDismiss={() => setDialogVisible(false)}
+                    />
                 </View>                
             </View>		
 		</>
