8329dc7242cccada6bc0e15e2139e8a668743de5
Ale110901 <alececchetto2001@gmail.com>
2024-08-27 17:55:28 +0200

    organizer/event initial setup

--- frontend/pages/organizer/Event.js
+++ frontend/pages/organizer/Event.js
@@ -1,13 +1,97 @@
-import React from 'react';
+import React, { useState, useEffect }from 'react';
 
-import { Text } from 'react-native-paper';
+import { View } from 'react-native';
+import { Button, Text, Icon } from 'react-native-paper';
 
-export default function EventPage({ route }) {
+import style from '../../style/custom.js';
+
+export default function EventPage({ navigation, route }) {
 	let params = route.params;
+    let [ dialogMessage, setDialogMessage ] = useState(null);
+
+    let title = 'title'
+    let description = 'description'
+    let date = 'date'
+    let start_time = 'start_time'
+    let end_time = 'end_time'
+    let price = 'price'
+    let capacity = 30
+    let reservations = 20
+
+    let [ booking, setBooking ] = useState(true);
+
+
+    const checkAvailability = (value) => {
+        if (value === capacity) { 
+          setBooking(false);
+        } else {
+          setBooking(true);
+        }
+    };
+
+    useEffect(() => {
+        checkAvailability(reservations);
+    }, [reservations]);
+
+
 
 	return (
 		<>
-			<Text>Event page! event_id={params.event_id}</Text>
+			<View style={ {alignSelf: 'center' }}>
+                <Text style={[ style.spaceBottom, style.mt20 ]}>
+                    <Icon source="format-title" size={20}/> 
+                    <Text style={{fontWeight: "bold"}} variant='headlineSmall'> Title: </Text>
+                    <Text variant="bodyLarge">{title}</Text>
+                </Text>
+
+                <Text style={[ style.spaceBottom ]}>
+                    <Icon source="text-box" size={20}/> 
+                    <Text style={{fontWeight: "bold"}} variant='headlineSmall'> Description: </Text>
+                    <Text variant="bodyLarge">{description}</Text>
+                </Text>
+
+                <Text style={[ style.spaceBottom ]}>
+                    <Icon source="calendar-text" size={20}/> 
+                    <Text style={{fontWeight: "bold"}} variant='headlineSmall'> Date: </Text>
+                    <Text variant="bodyLarge">{date}</Text>
+                </Text>
+
+                <Text style={[ style.spaceBottom ]}>
+                    <Icon source="clock-time-four" size={20}/> 
+                    <Text style={{fontWeight: "bold"}} variant='headlineSmall'> Time: </Text>
+                    <Text variant="bodyLarge">{start_time} - {end_time}</Text>
+                </Text>
+
+                <Text style={[ style.spaceBottom ]}>
+                    <Icon source="currency-usd" size={20}/> 
+                    <Text style={{fontWeight: "bold"}} variant='headlineSmall'> Price: </Text>
+                    <Text variant="bodyLarge">{price}€</Text>
+                </Text>
+
+                <Text style={[ style.spaceBottom ]}>
+                    <Icon source="account-group-outline" size={20}/> 
+                    <Text style={{fontWeight: "bold"}} variant='headlineSmall'> Actual reservations: </Text>
+                    <Text variant="bodyLarge">{reservations} / {capacity}</Text>
+                </Text>
+				<View style ={{flexDirection: 'row'}}>
+                    <Button
+                        title="modify"
+                        icon="pencil"
+                        mode="elevated"
+                        style={{ margin: 20 }}
+                        disabled={!booking}
+                        onPress={() => console.log('modify event')}
+                    >Modify</Button>
+                    <Button
+                        title="cancel"
+                        icon="close-circle-outline"
+                        mode="elevated"
+                        style={{ margin: 20 }}
+                        disabled={!booking}
+                        onPress={() => console.log('cancel event')}
+                    >Cancel</Button>
+                </View>                
+            </View>		
 		</>
 	);
 }
