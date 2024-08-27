0d3d208af8ac221d2e749222102488940f4297e0
Hackfront <hackfront@protonmail.ch>
2024-08-27 16:53:45 +0200

    Restyled participant/Profile page

--- frontend/pages/participant/Profile.js
+++ frontend/pages/participant/Profile.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 
-import { TextComponent, View } from 'react-native';
+import { ScrollView, View } from 'react-native';
 import { Text, Divider } from 'react-native-paper';
 
 import { doRequest } from '../../lib/rest.js';
@@ -9,13 +9,15 @@ import { ids as bsIds, styles as bsStyles } from '../../style/bootstrap.js';
 import style, { GLOBAL_SPACING } from '../../style/custom.js';
 
 export default function Profile({ navigation, route }) {
-  let part_name = 'Gino';
-
 	let { params } = route;
 
-  let [ part_spec ] = useState([]);
-
-  let [ setRefreshing ] = useState(false);
+  let [ userInfo, setUserInfo ] = useState({
+		name: 'Gino',
+		surname: 'Rossi',
+		email: 'prova@prova',
+		password: 'ciao',
+	});
+  let [ refreshing, setRefreshing ] = useState(false);
 
   async function doRefresh() {
     setRefreshing(true);
@@ -29,45 +31,45 @@ export default function Profile({ navigation, route }) {
     }
 
     if (response != null) {
-      part_spec(response);
+      setUserInfo(response);
     } else {
-      console.log('Error fetching participant')
+      console.log('Error fetching user')
     }
 
     setRefreshing(false);
   }
 
-  useEffect(() => {
-    doRefresh();
-  }, []);
+  // useEffect(() => {
+  //   doRefresh();
+  // }, []);
 
 	return (
-		<>
+		<ScrollView contentContainerStyle={style.box} style={bsStyles.container} dataSet={{ media: bsIds.container }}>
 			<Text variant="headlineMedium" style={[ style.box, {
 				alignSelf: 'center',
-			} ]}>Hi {part_name}, this is your profile!</Text>
+			} ]}>Hi {userInfo.name}, this is your profile!</Text>
 
-            <Divider style={[ style.spaceBottom ]}/>
+			<Divider style={[ style.spaceBottom ]}/>
 
-            <View style={ {alignSelf: 'center'} }>
-                <Text style={[ style.spaceBottom ]}>
-                    <Text style={{fontWeight: "bold"}}> Email: </Text>
-                    <Text>{part_spec.email}</Text>
-                </Text>
-                <Text style={[ style.spaceBottom ]}>
-                    <Text style={{fontWeight: "bold"}}> Password: </Text>
-                    <Text>{part_spec.password}</Text>
-                </Text>
-                <Text style={[ style.spaceBottom ]}>
-                    <Text style={{fontWeight: "bold"}}> Name: </Text>
-                    <Text>{part_spec.name}</Text>
-                </Text>
-                <Text>
-                    <Text style={{fontWeight: "bold"}}> Surname: </Text>
-                    <Text>{part_spec.surname}</Text>
-                </Text>
-            </View>
+			<View style={[ style.spaceBottom, { flexDirection: 'row' } ]}>
+				<Text style={{fontWeight: "bold"}}>Email: </Text>
+				<Text>{userInfo.email}</Text>
+			</View>
 
-		</>
+			<View style={[ style.spaceBottom, { flexDirection: 'row' } ]}>
+				<Text style={{fontWeight: "bold"}}>Password: </Text>
+				<Text>{userInfo.password}</Text>
+			</View>
+
+			<View style={[ style.spaceBottom, { flexDirection: 'row' } ]}>
+				<Text style={{fontWeight: "bold"}}>Name: </Text>
+				<Text>{userInfo.name}</Text>
+			</View>
+
+			<View style={[ style.spaceBottom, { flexDirection: 'row' } ]}>
+				<Text style={{fontWeight: "bold"}}>Surname: </Text>
+				<Text>{userInfo.surname}</Text>
+			</View>
+		</ScrollView>
 	);
 }
