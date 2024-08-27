33862e57ea21807dadadfaf67a2b4659ca3caee3
Hackfront <hackfront@protonmail.ch>
2024-08-27 16:53:29 +0200

    Restyled participant/Event page

--- frontend/pages/participant/Event.js
+++ frontend/pages/participant/Event.js
@@ -1,87 +1,72 @@
 import React, { useState, useEffect }from 'react';
 
-import { View } from 'react-native';
-import { Button, Text, Icon } from 'react-native-paper';
+import { ScrollView } from 'react-native';
+import { Button, Divider, Icon, Text } from 'react-native-paper';
 
+import { ids as bsIds, styles as bsStyles } from '../../style/bootstrap.js';
 import style from '../../style/custom.js';
 
 export default function EventPage({ navigation, route }) {
 	let params = route.params;
 
-    let title = 'title'
-    let description = 'description'
-    let date = 'date'
-    let start_time = 'start_time'
-    let end_time = 'end_time'
-    let price = 'price'
-    let capacity = 30
-    let reservations = 20
+	let [ eventInfo, setEventInfo ] = useState({
+		title: 'Event title',
+		description: 'Event description',
+		date: '01/12/2025',
+		start_time: '10:00',
+		end_time: '12:00',
+		price: '25 €',
+		capacity: 30,
+		reservations: 20,
+	});
 
-    let [ booking, setBooking ] = useState(true);
-
-
-    const checkAvailability = (value) => {
-        if (value === capacity) { 
-          setBooking(false);
-        } else {
-          setBooking(true);
-        }
-    };
-
-    useEffect(() => {
-        checkAvailability(reservations);
-    }, [reservations]);
-
-
+	let [ bookable, setBookable ] = useState(true);
 
 	return (
-		<>
-			<View style={ {alignSelf: 'center' }}>
-                <Text style={[ style.spaceBottom, style.mt20 ]}>
-                    <Icon source="format-title" size={20}/> 
-                    <Text style={{fontWeight: "bold"}} variant='headlineSmall'> title: </Text>
-                    <Text variant="bodyLarge">{title}</Text>
-                </Text>
+		<ScrollView contentContainerStyle={style.box} style={[ bsStyles.container ]} dataSet={{ media: bsIds.container }}>
+			<Text variant="headlineMedium" style={[ style.spaceBottom, style.mt20 ]}>{eventInfo.title}</Text>
 
-                <Text style={[ style.spaceBottom ]}>
-                    <Icon source="text-box" size={20}/> 
-                    <Text style={{fontWeight: "bold"}} variant='headlineSmall'> description: </Text>
-                    <Text variant="bodyLarge">{description}</Text>
-                </Text>
+      <Text style={[ style.spaceBottom ]}>
+        <Icon source="calendar-text" size={20} />
+        <Text style={{ fontWeight: 'bold', marginLeft: 20 }} variant='bodyLarge'>Date: </Text>
+        <Text variant="bodyLarge">{eventInfo.date}</Text>
+      </Text>
 
-                <Text style={[ style.spaceBottom ]}>
-                    <Icon source="calendar-text" size={20}/> 
-                    <Text style={{fontWeight: "bold"}} variant='headlineSmall'> date: </Text>
-                    <Text variant="bodyLarge">{date}</Text>
-                </Text>
+      <Text style={[ style.spaceBottom ]}>
+        <Icon source="clock-time-four" size={20} />
+        <Text style={{ fontWeight: 'bold', marginLeft: 20 }} variant='bodyLarge'>Time: </Text>
+        <Text variant="bodyLarge">{eventInfo.start_time} - {eventInfo.end_time}</Text>
+      </Text>
 
-                <Text style={[ style.spaceBottom ]}>
-                    <Icon source="clock-time-four" size={20}/> 
-                    <Text style={{fontWeight: "bold"}} variant='headlineSmall'> Time: </Text>
-                    <Text variant="bodyLarge">{start_time} - {end_time}</Text>
-                </Text>
+      <Text style={[ style.spaceBottom ]}>
+        <Icon source="currency-usd" size={20} />
+        <Text style={{ fontWeight: 'bold', marginLeft: 20 }} variant='bodyLarge'>Price: </Text>
+        <Text variant="bodyLarge">{eventInfo.price}</Text>
+      </Text>
 
-                <Text style={[ style.spaceBottom ]}>
-                    <Icon source="currency-usd" size={20}/> 
-                    <Text style={{fontWeight: "bold"}} variant='headlineSmall'> price: </Text>
-                    <Text variant="bodyLarge">{price}</Text>
-                </Text>
+      <Text style={[]}>
+        <Icon source="account-group-outline" size={20} />
+        <Text style={{ fontWeight: 'bold', marginLeft: 20 }} variant='bodyLarge'>Capacity: </Text>
+        <Text variant="bodyLarge">{eventInfo.eservations} / {eventInfo.capacity}</Text>
+      </Text>
 
-                <Text style={[ style.spaceBottom ]}>
-                    <Icon source="account-group-outline" size={20}/> 
-                    <Text style={{fontWeight: "bold"}} variant='headlineSmall'> capacity: </Text>
-                    <Text variant="bodyLarge">{reservations} / {capacity}</Text>
-                </Text>
+			<Divider style={[ style.spaceBottom, style.spaceTop ]} />
 
-				<Button
-					title="booking"
-					icon="book-arrow-right-outline"
-					mode="elevated"
-					style={{ margin: 20 }}
-                    disabled={!booking}
-					onPress={() => navigation.push('participant/Payment')}
-				>Book now!</Button>
-            </View>		
-		</>
+			<Text style={[ style.spaceBottom ]}>
+        <Icon source="text-box" size={20} />
+        <Text style={{ marginLeft: 20 }}>{eventInfo.description}</Text>
+      </Text>
+
+			<Button
+				title="booking"
+				icon="book-arrow-right-outline"
+				mode="elevated"
+				style={{ margin: 20 }}
+        disabled={!bookable}
+				onPress={() => navigation.push('participant/Payment', {
+					event_id: params.event_id,
+				})}
+			>Book now!</Button>
+    </ScrollView>
 	);
 }
