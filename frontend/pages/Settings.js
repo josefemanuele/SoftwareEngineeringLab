73ac3bfdef1c1fa232fa6784a0c91249296d68de
Hackfront <hackfront@protonmail.ch>
2024-08-28 15:46:04 +0200

    Add icon to settings page

--- frontend/pages/Settings.js
+++ frontend/pages/Settings.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { ScrollView, View } from 'react-native';
-import { Text } from 'react-native-paper';
+import { Icon, Text } from 'react-native-paper';
 
 import { ids as bsIds, styles as bsStyles } from '../style/bootstrap.js';
 import style from '../style/custom.js';
@@ -9,6 +9,10 @@ import style from '../style/custom.js';
 export default function Settings() {
 	return (
 		<ScrollView contentContainerStyle={style.box} style={bsStyles.container} dataSet={{ media: bsIds.container }}>
+			<View style={{ alignSelf: 'center', marginBottom: 25 }}>
+				<Icon source='cog' size={75}></Icon>
+			</View>
+
 			<Text>Settings!</Text>
 		</ScrollView>
 	)
