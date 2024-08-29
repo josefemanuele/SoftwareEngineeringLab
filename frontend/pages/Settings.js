0e005daf58d71361fddb91a76a1e6b1356337777
Hackfront <hackfront@protonmail.ch>
2024-08-29 15:07:01 +0200

    Add theme selection field in Settings

--- frontend/pages/Settings.js
+++ frontend/pages/Settings.js
@@ -1,19 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { ScrollView, View } from 'react-native';
 import { Icon, Text } from 'react-native-paper';
+import { Dropdown } from 'react-native-paper-dropdown';
 
 import { ids as bsIds, styles as bsStyles } from '../style/bootstrap.js';
 import style from '../style/custom.js';
 
+let themes = [
+	{ label: 'Follow system theme', value: 'system' },
+	{ label: 'Light', value: 'light' },
+	{ label: 'Dark', value: 'dark' },
+];
+
 export default function Settings() {
+	let [ theme, setTheme ] = useState('system');
+
 	return (
 		<ScrollView contentContainerStyle={style.box} style={bsStyles.container} dataSet={{ media: bsIds.container }}>
 			<View style={{ alignSelf: 'center', marginBottom: 25 }}>
 				<Icon source='cog' size={75}></Icon>
 			</View>
 
-			<Text>Settings!</Text>
+			{/* <View style={{ flexDirection: 'row' }}> */}
+				{/* <Text style={{ fontWeight: 'bold' }}>Theme:</Text> */}
+				<Dropdown
+					label="Theme"
+					value={theme}
+					onSelect={setTheme}
+					style={{ marginBottom: 20 }}
+					options={themes}
+				/>
+			{/* </View> */}
 		</ScrollView>
 	)
 }
