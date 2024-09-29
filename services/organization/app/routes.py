5f6cf8357d03a7bc891e0aa02a1dd4735f98b665
Josef Emanuele Zerpa Ruiz <zerparuiz.1837394@studenti.uniroma1.it>
2024-09-29 10:23:55 +0200

    Adapt Organization service to Rest contract

--- services/organization/app/routes.py
+++ services/organization/app/routes.py
@@ -2,11 +2,12 @@ from app import app
 from flask import jsonify, request
 
 organizations = {
-  1 : {'id' : 1, 'owner_id' : 2, 'name' : 'Sapienza'},
-  2 : {'id' : 2, 'owner_id' : 1, 'name' : 'Barber Shop'}
+  1 : {'id' : 1, 'owner_id' : 2, 'name' : 'Sapienza', 'phone' : '06 123456', 'address' : 'Piazzale Aldo Moro, 5', 'description' : 'The best university in Rome.', 'category' : 'Education'},
+  2 : {'id' : 2, 'owner_id' : 1, 'name' : 'Barber Shop', 'phone' : '06 123457', 'address' : 'Via Roma, 6', 'description' : 'The best barber in Rome.', 'category' : 'Beauty'}
 }
 id_counter = 2
 
+@app.route('/organizations', methods=['GET'])
 @app.route('/getall', methods=['GET'])
 def getAll():
     global organizations
@@ -18,12 +19,23 @@ def getByUser(user_id):
     match = {k : v for k, v in organizations.items() if v.get('owner_id') == user_id }
     return jsonify(match)
 
+@app.route('/organization/<int:id>', methods=['GET'])
+def getById(id):
+    global organizations
+    match = organizations.get(id)
+    return jsonify(match)
+
+@app.route('/organizations', methods=['POST'])
 @app.route('/create', methods=['POST'])
 def create():
     global organizations
     data = request.json
     name = data['name']
     owner_id = data['owner_id']
+    phone = data['phone']
+    address = data['address']
+    description = data['description']
+    category = data['category']
     match = {k : v for k, v in organizations.items() if v['name'] == name }
     if (len(match) == 0):
         global id_counter
@@ -35,7 +47,7 @@ def create():
         db.session.commit()
         print(UserSchema(many=True).dump(User.query.all()))
         '''
-        organization = { "id" : id_counter, "owner_id" : owner_id, 'name' : name }
+        organization = { "id" : id_counter, "owner_id" : owner_id, 'name' : name, 'phone' : phone, 'address' : address, 'description' : description, 'category' : category}
         organizations[id_counter] = {id_counter : organization }
         print(organizations)
         return jsonify(organization)
