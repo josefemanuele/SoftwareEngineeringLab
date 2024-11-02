7fe8e05f6f4de34d54599a0e1de790bec3dab0fc
Hackfront <hackfront@protonmail.ch>
2024-11-02 12:59:14 +0100

    Update services IP addresses in frontend

--- frontend/lib/constants.js
+++ frontend/lib/constants.js
@@ -1,7 +1,7 @@
 export const API_BASE_URLS =  {
-	event:        'http://localhost:8001',
-	organization: 'http://localhost:8002',
-	pay:          'http://localhost:8003',
-	reservations: 'http://localhost:8004',
-	user:         'http://localhost:8005',
+	event:        'http://10.234.0.3:5000',
+	organization: 'http://10.234.0.4:5000',
+	pay:          'http://10.234.0.5:5000',
+	reservations: 'http://10.234.0.6:5000',
+	user:         'http://10.234.0.7:5000',
 };
